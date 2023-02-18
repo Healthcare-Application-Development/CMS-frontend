83fd1910a2ca9b7685fea38a23e15d26524302cf
Prathviraj B N <prathviraj.b.n@gmail.com>
2023-02-18 18:31:06 +0530

    consent request screen added

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,6 @@ export { default as Header } from './Header/Header';
 export { default as Sidebar } from './Sidebar/Sidebar';
 export { default as SidebarItem } from './SidebarItem/SidebarItem';
 export { default as Dropdown } from './Dropdown/Dropdown';
+export { default as SearchBar } from './SearchBar/SearchBar';
+export { default as Button } from './Button/Button';
 
