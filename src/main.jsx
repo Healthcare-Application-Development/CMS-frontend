c032f0065145508f1839f0a36685252ba39fb969
prathviraj b <prathviraj.b.n@gmail.com>
2023-03-24 22:26:09 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,9 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
-      <div>
         <App />
-      </div>
     </Router>
   </React.StrictMode>
 );
